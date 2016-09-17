4e65a873b3cde00d37f18102a72b0a01720fa0c9
guoxiaojun <1016702891@qq.com>
2016-09-17 12:01:14 +0800

    add note

--- src/main/java/Sudo.java
+++ src/main/java/Sudo.java
@@ -96,7 +96,13 @@ public class Sudo {
     private JButton make;
     public JTextField[] fileds={t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,t13,t14,t15,t16,t17,t18,t19,t20,t21,t22,t23,t24,t25,t26,t27,t28,t29,t30,t31,t32,t33,t34,t35,t36,t37,t38,t39,t40,t41,
             t42,t43,t44,t45,t46,t47,t48,t49,t50,t51,t52,t53,t54,t55,t56,t57,t58,t59,t60,t61,t62,t63,t64,t65,t66,t67,t68,t69,t70,t71,t72,t73,t74,t75,t76,t77,t78,t79,t80,t81};
-
+    /*
+    * 实现原理是
+    * 1、先将一个随机数填入随机格子内，随机格子数也不重复
+    * 2、判断该格子横排是否有重复数字，如果没有，判断竖排是否有重复数字
+    * 3、如果横排或者竖排有重复数字，重新生成一个随机数，继续在横排和竖排内比较
+    * 4、其中getmatch方法有待进一步优化
+    * */
     Random random = new Random();
     List<Integer> listRandomIsExists=new ArrayList<Integer>();//随机格子
     public Sudo() {
@@ -187,7 +193,6 @@ public class Sudo {
     public int getMatch1(JTextField [][]arr1,int i,int x)
     {
         int result=0;
-        boolean isStopByRampant=true;
         boolean isStopByColumn=true;
         while(isStopByColumn)
         {
